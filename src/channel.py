e6f2d9dea46bf767cf03ed4e825ada73d9323826
Iaroslav <yaroslav.da@mail.ru>
2024-01-06 21:18:54 +0300

    создать методы класса Channel

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,18 @@
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+YT_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.YT_API_KEY = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(self.YT_API_KEY)
