d002c67a8d0d4643edb31b3f56448fa705611578
Iaroslav <yaroslav.da@mail.ru>
2024-01-14 14:22:42 +0300

    создать класс и методы

--- src/channel.py
+++ src/channel.py
@@ -1,18 +1,26 @@
+import json
 import os
 
 from dotenv import load_dotenv
+from googleapiclient.discovery import build
 
 load_dotenv()
 
-YT_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.YT_API_KEY = channel_id
+        self.channel_id = channel_id
+        api_key = os.getenv('YOUTUBE_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def printj(self, dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(self.YT_API_KEY)
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.printj(channel)
