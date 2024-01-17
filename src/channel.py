07e9fd8940659c7e4f8d2791edaa1c4a58c4dc55
Iaroslav <yaroslav.da@mail.ru>
2024-01-17 23:48:50 +0300

    добавить атрибуты класса

--- src/channel.py
+++ src/channel.py
@@ -15,6 +15,14 @@ class Channel:
         self.channel_id = channel_id
         api_key = os.getenv('YOUTUBE_API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube.com/channel/{channel_id}"
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
 
     def printj(self, dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
