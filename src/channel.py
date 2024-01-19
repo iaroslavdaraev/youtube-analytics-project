ffd5323483aafea36377c94430274b15a701260d
Iaroslav <yaroslav.da@mail.ru>
2024-01-19 14:56:23 +0300

    add str, add, sub, lt, le, gl, ge

--- src/channel.py
+++ src/channel.py
@@ -51,3 +51,24 @@ class Channel:
         with open(filename, "w", encoding="utf-8") as file:
             json.dump(data_info, file)
         return data_info
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
